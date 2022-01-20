480e496b07c04b57dcb43dc136a5e670b9eb16c3
nicocopy <nicocopy@hotmail.com>
2022-01-20 16:57:25 -0300

    cam

---
+++ validacion.js
@@ -0,0 +1,12 @@
+var alfabeto =" abcdefghijklmnopqrstuvwxyz";
+
+function validacionTextoIngresado(texto){
+    var textoCorrecto = false;
+    for(i=0;i<texto.length;i++){
+        textoCorrecto = alfabeto.includes(texto[i]);
+        if(!textoCorrecto){
+            break;
+        }
+    }
+    return textoCorrecto;
+}
